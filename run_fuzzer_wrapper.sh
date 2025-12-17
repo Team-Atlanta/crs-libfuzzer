4c175e23fc24536dd2227286ddc37eb71117fda9
Hanqing ZHao <hanqing@gatech.edu>
2025-12-17 20:07:28 +0000

    fix crsbench interfaces

--- run_fuzzer_wrapper.sh
+++ run_fuzzer_wrapper.sh
@@ -3,4 +3,4 @@
 set -eu
 
 NCPU=$(echo "$CPUSET_CPUS" | awk -F',' '{print NF}')
-taskset -c $CPUSET_CPUS run_fuzzer $@ --fork=$NCPU
+taskset -c $CPUSET_CPUS run_fuzzer $@ --fork=$NCPU -artifact_prefix=/out/povs/ /out/corpus
